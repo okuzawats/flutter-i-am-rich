1988758132c3a7eb3195d7d3d4fc4e60986191b1
okuzawats <okuzawats@gmail.com>
2020-01-03 10:31:56 +0900

    Format

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Hello World'),
+        ),
       ),
-    ));
+    );
